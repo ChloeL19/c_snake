612033c5b77bdb816376cf3a652e1bca204ec26b
Chloe Loughridge <cloughridge@college.harvard.edu>
2020-05-20 12:28:54 -1000

    added to snake_env, borrowing a lot from different source

--- c_snake/envs/snake_env.py
+++ c_snake/envs/snake_env.py
@@ -66,12 +66,30 @@ class SnakeEnv(gym.Env):
   def step(self, action):
     self.last_obs, rewards, done, info = self.controller.step(action)
     return self.last_obs, rewards, done, info
-    
+
   def reset(self):
-    pass
-  def render(self, mode='human'):
-    # consider using openai classic control rendering script?
-    # https://github.com/openai/gym/blob/master/gym/envs/classic_control/rendering.py
-    print("hey")
+    self.controller = Controller(self.grid_size, self.unit_size, self.unit_gap, self.snake_size, 
+      self.n_snakes, self.n_foods, random_init=self.random_init)
+    self.last_obs = self.controller.grid.grid.copy()
+    return self.last_obs
+
+  def render(self, mode='human', close=False, frame_speed = 0.1):
+    if self.viewer is None:
+        self.fig = plt.figure()
+        self.viewer = self.fig.add_subplot(111)
+        plt.ion()
+        self.fig.show()
+    else:
+        self.viewer.clear()
+        self.viewer.imshow(self.last_obs)
+        plt.pause(frame_speed)
+    self.fig.canvas.draw()
+    
   def close(self):
     pass
+
+
+
+
+
+
